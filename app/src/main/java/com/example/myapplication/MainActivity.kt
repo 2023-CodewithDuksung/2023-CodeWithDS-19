bae0f1f998db2713986c2158ed6008c1877bcfba
Kang Chaewon <marie9609@naver.com>
2023-08-21 23:45:01 +0900

    add splash

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import com.example.myapplication.HomeFragment
 import com.example.myapplication.R
@@ -13,8 +15,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomNavigationView: BottomNavigationView
 
-    private val HomeFragment = HomeFragment()
-    private val TalkFragment = TalkFragment()
+//    private val HomeFragment = HomeFragment()
+//    private val TalkFragment = TalkFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +25,6 @@ class MainActivity : AppCompatActivity() {
         // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
         var bnv_main = findViewById(R.id.bottomNavi) as BottomNavigationView
 
-        // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
-        // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
         bnv_main.run { setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.nav1 -> {
@@ -45,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         }
             selectedItemId = R.id.nav1
         }
+
+//        val intent = Intent(this, AuthActivity::class.java)
+//        startActivity(intent)
+
     }
     private fun switchFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
