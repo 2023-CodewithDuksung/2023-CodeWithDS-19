32147ccab84a584726ea1c86075866ebeb16eb6d
somin <xmin0811@naver.com>
2023-08-23 20:24:19 +0900

    플로팅버튼으로 PostActivity 호출

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -23,8 +23,9 @@ class MainActivity : AppCompatActivity() {
     private val Fragment_2 = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         //버튼
         findViewById<View>(R.id.lButton).setOnClickListener {
